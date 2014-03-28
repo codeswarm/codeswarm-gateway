de9d60a7dc70a4086ebb867c675d754edc36e0ce
Pedro Teixeira <i@pgte.me>
2014-03-28 16:32:27 +0000

    removed logging

--- injector/types/qunit/plugin.js
+++ injector/types/qunit/plugin.js
@@ -11,7 +11,6 @@
   QUnit.log(function(res) {
     if (res && ! res.result) {
       var err = JSON.stringify(res);
-      console.log({error: err})
       state.results.errors.push(err);
     }
   });
@@ -19,7 +18,6 @@
   QUnit.done(function(results){
     state.ended = true;
     state.results.results = results;
-    console.log(state);
   });
 
 })();
